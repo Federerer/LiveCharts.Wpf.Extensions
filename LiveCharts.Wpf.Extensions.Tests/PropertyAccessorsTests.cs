ee1a21864221c9ad4924145cc0ceadbdde34cca1
Federerer <08fedra@interia.pl>
2017-02-27 22:35:23 +0100

    Modified getters and unit tests

--- LiveCharts.Wpf.Extensions.Tests/PropertyAccessorsTests.cs
+++ LiveCharts.Wpf.Extensions.Tests/PropertyAccessorsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LiveCharts.Wpf.Extensions.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LiveCharts.Wpf.Extensions.Tests
@@ -6,51 +7,102 @@ namespace LiveCharts.Wpf.Extensions.Tests
     [TestClass]
     public class PropertyAccessorsTests
     {
+        #region Weakly typed
+
         [TestMethod]
-        public void CreateGetter_ValidProperty_ReturnsGetter()
+        public void CreateGetter_ReferenceTypeWithValidPropertyName_ReturnsGetter()
         {
             var data = new TestData<int>("Test", 5);
-            var getter = PropertyAccessors.PropertyAccessors.CreateGetter<TestData<int>>("Name");
+            var getter = PropertyAccessors.CreateGetter("Name", data);
 
             Assert.IsNotNull(getter);
         }
 
         [TestMethod]
-        public void CreateGetter_ValidProperty_ReturnsValueType()
+        public void CreateGetter_ValueTypeWithValidPropertyName_ReturnsGetter()
+        {
+            var data = TimeSpan.FromDays(1);
+            var getter = PropertyAccessors.CreateGetter(nameof(TimeSpan.TotalMilliseconds), data);
+
+            Assert.IsNotNull(getter);
+        }
+
+        [TestMethod]
+        public void CreateGetter_GetterCalled_ReturnsValueType()
         {
             var data = new TestData<int>("Test", 5);
-            var getter = PropertyAccessors.PropertyAccessors.CreateGetter<TestData<int>>("Value");
+            var getter = PropertyAccessors.CreateGetter("Value", data);
 
             Assert.AreEqual(5, getter(data));
         }
 
         [TestMethod]
-        public void CreateGetter_ValidProperty_ReturnsRefferenceType()
+        public void CreateGetter_GetterCalled_ReturnsRefferenceType()
         {
-            var refference = new object();
-            var data = new TestData<object>("Test", refference);
-            var getter = PropertyAccessors.PropertyAccessors.CreateGetter<TestData<object>>("Value");
+            var data = new TestData<object>("Test", null);
+            var getter = PropertyAccessors.CreateGetter("Name", data);
 
-            Assert.AreSame(refference, getter(data));
+            Assert.AreSame(data.Name, getter(data));
         }
 
         [TestMethod]
         public void CreateGetter_InvalidProperty_ReturnsNull()
         {
             var data = new TestData<int>("Test", 5);
-            var getter = PropertyAccessors.PropertyAccessors.CreateGetter<TestData<int>>("Invalid");
+            var getter = PropertyAccessors.CreateGetter("Invalid", data);
 
             Assert.IsNull(getter);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidCastException))]
-        public void CreateGetter_InvalidObject_ThrowsException()
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateGetter_GetterCalledWithInvalidObjectType_ThrowsArgumentException()
         {
             var data = new TestData<int>("Test", 5);
-            var getter = PropertyAccessors.PropertyAccessors.CreateGetter<TestData<int>>("Value");
+            var getter = PropertyAccessors.CreateGetter("Value", data);
 
             getter(new object());
         }
+
+        #endregion
+        #region Strongly typed
+
+        [TestMethod]
+        public void CreateGetterT_ValidReturnType_ReturnsGetter()
+        {
+            var data = new TestData<int>("Test", 5);
+            var getter = PropertyAccessors.CreateGetter<int>("Value", data);
+
+            Assert.IsNotNull(getter);
+        }
+
+        [TestMethod]
+        public void CreateGetterT_InvalidReturnType_ReturnsNull()
+        {
+            var data = new TestData<int>("Test", 5);
+            var getter = PropertyAccessors.CreateGetter<string>("Value", data);
+
+            Assert.IsNull(getter);
+        }
+
+        [TestMethod]
+        public void CreateGetterT_GetterCalled_ReturnsValueType()
+        {
+            var data = new TestData<int>("Test", 5);
+            var getter = PropertyAccessors.CreateGetter<int>("Value", data);
+
+            Assert.AreEqual(5, getter(data));
+        }
+
+        [TestMethod]
+        public void CreateGetterT_GetterCalled_ReturnsReferenceType()
+        {
+            var data = new TestData<int>("Test", 5);
+            var getter = PropertyAccessors.CreateGetter<string>("Name", data);
+
+            Assert.AreSame(data.Name, getter(data));
+        }
+
+        #endregion
     }
 }
---
+++ LiveCharts.Wpf.Extensions/Utils/PropertyAccessors.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCharts.Wpf.Extensions.Utils
+{
+    public static class PropertyAccessors
+    {
+        public static Func<object, object> CreateGetter(string propertyName, object target)
+        {
+            return CreateGetter<object>(propertyName, target.GetType());
+        }
+
+        public static Func<object, object> CreateGetter(string propertyName, Type type)
+        {
+            return CreateGetter<object>(propertyName, type);
+        }
+
+        public static Func<object, T> CreateGetter<T>(string propertyName, object target)
+        {
+            return CreateGetter<T>(propertyName, target.GetType());
+        }
+
+        public static Func<object, T> CreateGetter<T>(string propertyName, Type type)
+        {
+            var property = type.GetProperty(propertyName);
+            if (property == null || !typeof(T).IsAssignableFrom(property.PropertyType))
+            {
+                return null;
+            }
+            var helper = typeof(PropertyAccessorsHelper<,,>).MakeGenericType(type, property.PropertyType, typeof(T));
+
+            var method = helper.GetMethod(nameof(PropertyAccessorsHelper<object, object, object>.CreateGetterInternal), BindingFlags.NonPublic | BindingFlags.Static);
+            return (Func<object, T>)method.Invoke(null, new object[] { property });
+
+        }
+
+        public static Action<object> CreateSetter<TData>(string propertyName)
+        {
+            return o => new object();
+        }
+    }
+
+    internal class PropertyAccessorsHelper<TData, TValue, TReturn> where TValue : TReturn
+    {
+        delegate TValue RefFunc(ref TData o);
+
+        internal static Func<object, TReturn> CreateGetterInternal(PropertyInfo property)
+        {
+            var info = property.GetGetMethod();
+            if (info.DeclaringType.IsValueType)
+            {
+                var g = (RefFunc)Delegate.CreateDelegate(typeof(RefFunc), info);
+                return o =>
+                {
+                    var data = (TData)o;
+                    return g(ref data);
+                };
+            }
+
+            var getter = (Func<TData, TValue>)Delegate.CreateDelegate(typeof(Func<TData, TValue>), info);
+            return o =>
+            {
+                if (!(o is TData))
+                {
+                    throw new ArgumentException();
+                }
+                return getter((TData) o);
+            };
+        }
+    }
+}
